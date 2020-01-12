169070744d70b5b5451f6556da40a3afacb93ca1
Łukasz Korecki <lukasz@korecki.me>
2020-01-12 01:04:44 +0000

    Create new snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomnom/utility-belt.sql "0.3.1-SNAPSHOT"
+(defproject nomnom/utility-belt.sql "1.0.0-SNAPSHOT"
   :description "Tools for working with Postgres (queries, connection pool component, helpers etc)"
   :url "https://github.com/nomnom-insights/nomnom.utility-belt.sql"
   :deploy-repositories {"clojars" {:sign-releases false
