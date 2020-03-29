be896eae7dbd0bfd2dbfe9e4509c9a769e52f219
Łukasz Korecki <lukasz@korecki.me>
2020-03-28 17:43:17 -0700

    Test reading json_agg results for arrays

--- test/utility_belt/sql/core_test.clj
+++ test/utility_belt/sql/core_test.clj
@@ -1,24 +1,30 @@
 (ns utility-belt.sql.core-test
-  (:require [clojure.test :refer :all]
-            ;; -- test helpers
-            [utility-belt.sql.connection :as connection]
-            [utility-belt.sql.people :as people]
-            ;; -- actual things under test
-            [utility-belt.time :as time]
-            [utility-belt.sql.conv]
-            [utility-belt.sql.helpers :as helpers]
-            [utility-belt.sql.model :as model]))
+  (:require
+    [clojure.test :refer :all]
+    ;; -- test helpers
+    [utility-belt.sql.connection :as connection]
+    [utility-belt.sql.conv]
+    [utility-belt.sql.helpers :as helpers]
+    [utility-belt.sql.model :as model]
+    [utility-belt.sql.people :as people]
+    ;; -- actual things under test
+    [utility-belt.time :as time]))
+
 
 (def conn (atom nil))
+
 
 (use-fixtures :each (fn [test-fn]
                       (connection/start! conn)
                       (try
                         (people/setup* @conn)
+                        (people/setup-squad* @conn)
+                        (people/setup-users* @conn)
                         (people/delete-all* @conn)
                         (test-fn)
                         (finally
                           (connection/stop! conn)))))
+
 
 (deftest crud-operations
   (is (= [] (people/get-all* @conn)))
@@ -58,14 +64,14 @@
            :attributes {:bar 1
                         :foo ["a" "b" "c"  "92731758-98f9-4358-974b-b15c74c917d9"]}
            :confirmed-at #inst "2019-06-24"}]
-         (people/get-all* @conn)))
+         (map #(dissoc % :id) (people/get-all* @conn))))
   (is (= [{:name "yest"
            :email "test@test.com"
            :entity-id  #uuid "92731758-98f9-4358-974b-b15c74c917d9"
            :attributes {:bar 1
                         :foo ["a" "b" "c"  "92731758-98f9-4358-974b-b15c74c917d9"]}
            :confirmed-at #inst "2019-06-24"}]
-         (people/get-all* @conn {:email "test@test.com"})))
+         (map #(dissoc % :id) (people/get-all* @conn {:email "test@test.com"}))))
   (is (= 1
          (people/set-email* @conn {:old-email "test@test.com"
                                    :new-email "test2@test.com"})))
@@ -73,3 +79,17 @@
          (people/delete* @conn {:email "dat@test.com"})))
   (is (= {:count 1}
          (people/count-all* @conn))))
+
+
+(deftest array-coercions
+  (people/add-user* @conn {:name "yest"})
+  (people/add-user* @conn {:name "who"})
+  (let [people (people/get-users* @conn)]
+    (mapv #(people/add-to-squad* @conn {:name "test" :user-id (:id %)}) people)
+    (is (= 1 (count (people/get-squad* @conn {:name "test"}))))
+    (is (= {:name "test"
+            :peeps #{{:name "yest"}
+                     {:name "who"}}}
+           (-> (people/get-squad* @conn {:name "test"})
+               first
+               (update :peeps (fn [p] (set (map #(dissoc % :id) p)))))))))
--- test/utility_belt/sql/people.sql
+++ test/utility_belt/sql/people.sql
@@ -11,10 +11,34 @@ attributes jsonb,
 created_at timestamp default current_timestamp,
 updated_at timestamp default current_timestamp,
 confirmed_at timestamp
+);
+
+
+-- :name setup-squad* :!
+
+create table if not exists squad (
+name text not null,
+user_id integer not null
 )
 
+-- :name setup-users* :!
+
+create table if not exists users (
+id serial primary key,
+name text not null
+)
+
+
+-- :name delete-all* :! :*
+truncate people;
+truncate users;
+truncate squad;
+
+
 -- :name get-all* :? :*
-select name, email, attributes, confirmed_at, entity_id from people
+select
+
+id, name, email, attributes, confirmed_at, entity_id from people
 
 --~ (when (:email params) "where email = :email")
 order by id desc;
@@ -29,6 +53,8 @@ values
 (:name, :email, CAST(:attributes AS jsonb), current_timestamp, :confirmed-at, :entity-id)
 returning name, email, attributes, confirmed_at, entity_id
 
+
+
 -- :name set-email* :! :n
 update people
 set email = :new-email
@@ -40,5 +66,31 @@ delete from people
 where email = :email
 
 
--- :name delete-all* :! :*
-truncate people
+-- :name add-user* :!
+insert into users (name) values (:name);
+
+-- :name get-users* :? :*
+select * from users;
+
+-- :name add-to-squad* :!
+insert into squad
+(name, user_id)
+
+values
+(:name, :user-id)
+
+
+-- :name get-squad* :1 :?
+select
+  s.name,
+  json_agg(u.*) as peeps
+
+from squad s
+
+join users u on (s.user_id = u.id)
+
+where
+
+s.name = :name
+
+group by s.name
