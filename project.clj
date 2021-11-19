f25b7faef3c90c484584e962ff3e01100d144ac1
Łukasz Korecki <lukasz@korecki.me>
2021-11-19 12:08:47 -0800

    New snapshot version, update dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomnom/utility-belt.sql "1.0.1"
+(defproject nomnom/utility-belt.sql "1.1.0-SNAPSHOT"
   :description "Tools for working with Postgres (queries, connection pool component, helpers etc)"
   :url "https://github.com/nomnom-insights/nomnom.utility-belt.sql"
   :deploy-repositories {"clojars" {:sign-releases false
@@ -7,8 +7,8 @@
 
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.postgresql/postgresql "42.2.23"]
-                 [com.github.seancorfield/next.jdbc "1.2.689"]
+                 [org.postgresql/postgresql "42.3.1"]
+                 [com.github.seancorfield/next.jdbc "1.2.753"]
                  [cheshire "5.10.1"]
                  [clj-time "0.15.2"]
                  [com.layerware/hugsql "0.5.1"]
@@ -17,6 +17,6 @@
   :plugins [[lein-cloverage "1.1.1" :exclusions [org.clojure/clojure]]]
   :profiles {:dev
              {:dependencies [[org.clojure/tools.logging "1.1.0"]
-                             [ch.qos.logback/logback-classic "1.2.5"]
+                             [ch.qos.logback/logback-classic "1.2.7"]
                              [nomnom/utility-belt "1.3.1"]
                              [com.stuartsierra/component "1.0.0"]]}})
