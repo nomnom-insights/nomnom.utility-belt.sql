89d1887aad1e445a0a39b3df2c463b90bd29b05d
Łukasz Korecki <lukasz@korecki.me>
2021-11-19 12:40:36 -0800

    Adds a test + helpers for detecting if a connection is a transaction or not

--- src/utility_belt/sql/helpers.clj
+++ src/utility_belt/sql/helpers.clj
@@ -1,11 +1,14 @@
 (ns utility-belt.sql.helpers
-  (:require [next.jdbc]
-            [utility-belt.sql.model :as model]))
+  (:require
+    [next.jdbc]
+    [utility-belt.sql.model :as model]))
+
 
 (defmacro with-transaction
   "Wrapper around jdbc transaction macro. Cleans up imports basically"
   [binding & body]
   `(next.jdbc/with-transaction ~binding ~@body))
+
 
 (defn execute
   ([connection statement]
@@ -13,3 +16,11 @@
   ([connection statement {:keys [mode]}]
    (next.jdbc/execute! connection statement
                        (get model/modes mode))))
+
+
+(defn transaction?
+  "Detects if passed in data source is a transaction or not.
+  NOTE: this only works with HikariCP managed connections/transactions and the connection pool
+  defined in utility-belt.sql.component.connection-pool"
+  [tx-or-conn]
+  (-> tx-or-conn bean :transactionIsolation number?))
--- test/utility_belt/sql/helpers_test.clj
+++ test/utility_belt/sql/helpers_test.clj
@@ -1,14 +1,17 @@
 (ns utility-belt.sql.helpers-test
-  (:require [clojure.test :refer :all]
+  (:require
+    [clj-time.coerce :as coerce]
+    [clojure.test :refer [deftest testing is use-fixtures]]
     ;; -- test helpers
-            [utility-belt.sql.connection :as connection]
-            [utility-belt.sql.people :as people]
+    [utility-belt.sql.connection :as connection]
     ;; -- actual things under test
-            [utility-belt.sql.conv]
-            [utility-belt.sql.helpers :as helpers]
-            [clj-time.coerce :as coerce]))
+    [utility-belt.sql.conv]
+    [utility-belt.sql.helpers :as helpers]
+    [utility-belt.sql.people :as people]))
+
 
 (def conn (atom nil))
+
 
 (use-fixtures :each (fn [test-fn]
                       (connection/start! conn)
@@ -20,6 +23,7 @@
                         (test-fn)
                         (finally
                           (connection/stop! conn)))))
+
 
 (deftest raw-execute
   (people/add* @conn {:name "yest"
@@ -34,32 +38,40 @@
            :email "test@test.com"
            :name "yest"}]
          (map
-          #(dissoc %
-                   :id
-                   :created-at
-                   :updated-at)
-          (helpers/execute @conn ["select * from people"])))))
+           #(dissoc %
+                    :id
+                    :created-at
+                    :updated-at)
+           (helpers/execute @conn ["select * from people"])))))
+
 
 (deftest transaction-operation
-  (helpers/with-transaction [tx @conn]
-    (people/add* tx {:name "yest"
-                     :email "test@test.com"
-                     :entity-id  #uuid "92731758-98f9-4358-974b-b15c74c917d9"
-                     :attributes {:bar 1
-                                  :foo [:a :b :c]}
-                     :confirmed-at #inst "2019-02-03"})
-    (people/add* tx {:name "who"
-                     :email "dat@test.com"
-                     :entity-id  #uuid "92731758-98f9-4358-974b-b15c74c917d9"
-                     :attributes {:bar 1
-                                  :foo {:ok :dawg}}
-                     :confirmed-at #inst "2019-02-03"})
-    (testing  "tx not finished yet so using db-pool no results should be reutnred"
-      (is (= 0 (count (people/get-all* @conn)))))
-    (testing "when reading via transaction, we get the correct result"
-      (is (= 2 (count (people/get-all* tx))))))
-  (testing "tx is done, we can read via normal conn:"
-    (is (= 2 (count (people/get-all* @conn)))))
+  (testing "detecting transactions"
+    (helpers/with-transaction [tx @conn]
+      (is (true? (helpers/transaction? tx)))
+      ;; need to run something to avoid "connection closed" error
+      (helpers/execute tx ["select * from now()"]))
+    (is (false? (helpers/transaction? @conn))))
+  (testing "operations in a transaction"
+    (helpers/with-transaction [tx @conn]
+      (people/add* tx {:name "yest"
+                       :email "test@test.com"
+                       :entity-id  #uuid "92731758-98f9-4358-974b-b15c74c917d9"
+                       :attributes {:bar 1
+                                    :foo [:a :b :c]}
+                       :confirmed-at #inst "2019-02-03"})
+      (people/add* tx {:name "who"
+                       :email "dat@test.com"
+                       :entity-id  #uuid "92731758-98f9-4358-974b-b15c74c917d9"
+                       :attributes {:bar 1
+                                    :foo {:ok :dawg}}
+                       :confirmed-at #inst "2019-02-03"})
+      (testing  "tx not finished yet so using db-pool no results should be reutnred"
+        (is (= 0 (count (people/get-all* @conn)))))
+      (testing "when reading via transaction, we get the correct result"
+        (is (= 2 (count (people/get-all* tx))))))
+    (testing "tx is done, we can read via normal conn:"
+      (is (= 2 (count (people/get-all* @conn))))))
 
   (testing "failing within transaction - should not add rows if an exception is thrown during transaction"
     (helpers/with-transaction [tx @conn {:rollback-only true}]
@@ -79,6 +91,7 @@
                  (ex-message e))))))
     (testing "No new data should be inserted"
       (is (= 2 (count (people/get-all* @conn)))))))
+
 
 (deftest modes-test
   (people/add* @conn {:name "yest"
